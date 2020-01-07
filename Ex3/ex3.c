48d05c18939508eed7d4b85ceb53c86b7fd4da1b
psyroces <56292652+psyroces@users.noreply.github.com>
2020-01-07 00:23:22 -0800

    Update ex3.c

--- Ex3/ex3.c
+++ Ex3/ex3.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+int a;
+int b,min;
+int n,i;
+scanf("%d",&n);
+scanf("%d",&a);
+min=a;
+for(i=0;i<n;i++){
+scanf("%d",&b);
+if(b<a)
+min=b;}
+printf("%d",min);
+}
